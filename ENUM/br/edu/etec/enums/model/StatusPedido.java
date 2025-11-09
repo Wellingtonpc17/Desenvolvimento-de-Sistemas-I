bf66555a610a5fdd629a377639e8530b4d7824f8
Wellingtonpc17 <engwellingtoncavalcanti@gmail.com>
2025-11-09 19:09:22 -0300

    Delete ENUM/br/edu/etec/enums directory

--- ENUM/br/edu/etec/enums/model/StatusPedido.java
+++
@@ -1,9 +0,0 @@
-package br.edu.etec.enums.model;
-
-// Enum que representa os possíveis status de um pedido
-public enum StatusPedido {
-    NOVO,          // Pedido recém-criado
-    PROCESSANDO,   // Pedido em andamento
-    ENTREGUE,      // Pedido entregue ao cliente
-    CANCELADO      // Pedido cancelado
-}
